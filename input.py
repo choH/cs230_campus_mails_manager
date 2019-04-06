c4840b26bfd44a52c127e8f5cf7a55f985675936
HarryDunham <hdunham20@wooster.edu>
2019-04-06 16:33:41 -0400

    Full User Interface implementation. With basic functionality for submission and carrier selection

--- input.py
+++ input.py
@@ -1,14 +1,34 @@
 from kivy.app import App
+import kivy
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.config import Config
-# Config.set('graphics', 'width', '323')
-# Config.set('graphics', 'height', '700')
+#Config.set('graphics', 'width', '323')
+#Config.set('graphics', 'height', '700')
 
 
 class Input(Screen):
     def submission_func(self):
-        for text_input in self.ids:
+        list_of_inputs = [self.ids.tracking_number, self.ids.student_name,
+                          self.ids.description, self.ids.bin_number,
+                          self.ids.box_number, self.ids.staff_id,
+                          self.ids.date_received]
+
+        filename = "registered.data"
+        try:
+            open(filename, "w")
+        except:
+            print("File failed to open")
+
+
+        for text_input in list_of_inputs:
             pass
+
+    def carrier_func(self, carrier_id):
+        carrier_dict = {1: self.ids.USPS, 2: self.ids.Fedex, 3: self.ids.UPS,
+                        4: self.ids.DHL, 5: self.ids.Other}
+        id = carrier_dict[carrier_id]
+        id.background_color = (0, 1, 0, 1)
+
 
 
 class InputApp(App):
