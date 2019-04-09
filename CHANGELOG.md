c550ea3515089982d2fb3dcfeea3c521b1439f2c
HarryDunham <hdunham20@wooster.edu>
2019-04-09 16:57:05 -0400

    Updated description of the initial iteration of the input screen

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,25 @@ includes basic text inputs and a non-functioning submit
 button (inputs include: tracking number, current date, 
 bin number, etc.) . This is meant to get a visual 
 understanding of what the input screen should look like.
+
+>4/6/19, Harry Dunham:
+Implemented the entire UI for the screen. This entailed the creation of
+a grid layout that encompasses the entire screen. This screen is divided into
+three BoxLayouts. The first is a simple Status button that displays whether
+package submission was successful. Next, is the input fields for the package, as
+mentioned before this has: tracking number textinput, staff-id, etc.
+Additionally, a selection system was added at the bottom  of the input fields
+for the carrier id. It is simply 5 buttons arrayed in a line that allows the
+user to select the package's carrier.
+
+> 4/8/19, Harry Dunham:
+Fixed the carrier implementation so that whenever one carrier is
+selected all the other carriers are deselected. Also if the user selects the
+currently selected carrier it will deselect that carrier.
+
+>4/9/19, Harry Dunham:
+Implementation of the JSON file reader/writer and an actual submission button
+function. Additionally, the status button was replaced by a progressbar,
+which isn't as practical when considering that this project modifies a local file,
+however it is a mockup of the application should it ever have an actual
+website backend and server support.
--- input.py
+++ input.py
@@ -1,33 +1,108 @@
+import json
 from kivy.app import App
-import kivy
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from kivy.config import Config
-#Config.set('graphics', 'width', '323')
-#Config.set('graphics', 'height', '700')
+from datetime import datetime
+# datetime.utcnow()
+# Config.set('graphics', 'width', '323')
+# Config.set('graphics', 'height', '700')
 
 
 class Input(Screen):
     def submission_func(self):
-        list_of_inputs = [self.ids.tracking_number, self.ids.student_name,
-                          self.ids.description, self.ids.bin_number,
-                          self.ids.box_number, self.ids.staff_id,
-                          self.ids.date_received]
+        self.ids.status.value = 10
+        selected_carrier = ''
+        carrier_list = [self.ids.USPS, self.ids.Fedex, self.ids.UPS,
+                        self.ids.DHL, self.ids.Other]
+        for carrier in carrier_list:
+            self.ids.status.value = self.ids.status.value + 1
+            if carrier.background_color == [0, 0, 0, 1]:
+                selected_carrier = carrier.text
 
+        if selected_carrier == '':
+            self.popup("Please select a mail carrier")
+            self.ids.status.value = 0
+            return
+        information = {'tracking_num': self.ids.tracking_num.text, 'recipient':
+                       self.ids.recipient.text, 'note': self.ids.note.text,
+                       'bin_number': self.ids.bin_number.text, 'box_number':
+                       self.ids.box_number.text, 'registered_staff':
+                       self.ids.registered_staff.text, 'delivered_time':
+                       self.ids.delivered_time.text, 'is_redeemed': 'false',
+                       'redeemed_time': 'false', 'service': selected_carrier}
+
+        for json_name, text in information.items():
+            if text == '':
+                self.popup("Please fill the {} text field".format(json_name))
+                self.ids.status.value = 0
+                return
+
+        self.ids.status.value = self.ids.status.value + 10
         filename = "registered.data"
-        try:
-            open(filename, "w")
-        except:
-            print("File failed to open")
+        with open(filename, "r") as copy_file:
+            try:
+                data = json.load(copy_file)
+            except ValueError:
+                data = []
 
+            data.append(information)
+        self.ids.status.value = self.ids.status.value + 25
+        with open(filename, "w") as json_file:
+            json.dump(data, json_file)
+            json_file.close()
 
-        for text_input in list_of_inputs:
-            pass
+        self.ids.status.value = self.ids.status.value + 25
+        self.ids.tracking_num.text = ''
+        self.ids.recipient.text = ''
+        self.ids.note.text = ''
+        self.ids.bin_number.text = ''
+        self.ids.box_number.text = ''
+        self.ids.registered_staff.text = ''
+        self.ids.delivered_time.text = ''
+
+        self.ids.status.value = self.ids.status.value + 20
+
+        for carrier in carrier_list:
+            self.ids.status.value = self.ids.status.value + 1
+            carrier.background_color = [1, 1, 1, 1]
+
+    def popup(self, error_message):
+        """
+        This function implements popup widget functionality, whereby should an
+        exception be thrown, a popup will be created to alert the user the
+        error. The user then must acknowledge the popup by clicking the 'Ok'
+        button. This physical instantiation of this includes a general
+        BoxLayout for the entire popup, a Label widget to display the error
+        message, and an 'Ok' button to accept the error.
+
+        :param error_message: The exceptions error message, which will be
+        displayed by the popup.
+        :return: None
+        """
+        content = BoxLayout(orientation='vertical')
+        error_label = Label(text=error_message)
+        dismiss_button = Button(text='Ok')
+        content.add_widget(error_label)
+        content.add_widget(dismiss_button)
+        popup = Popup(title='Error', content=content, size_hint=(.3, .25),
+                      auto_dismiss=False)
+        dismiss_button.bind(on_press=popup.dismiss)
+        popup.open()
 
     def carrier_func(self, carrier_id):
         carrier_dict = {1: self.ids.USPS, 2: self.ids.Fedex, 3: self.ids.UPS,
                         4: self.ids.DHL, 5: self.ids.Other}
         id = carrier_dict[carrier_id]
-        id.background_color = (0, 1, 0, 1)
+        if id.background_color == [0, 0, 0, 1]:
+            id.background_color = [1, 1, 1, 1]
+        else:
+            for index in carrier_dict:
+                carrier_dict[index].background_color = [1, 1, 1, 1]
+            id.background_color = [0, 0, 0, 1]
 
 
 
